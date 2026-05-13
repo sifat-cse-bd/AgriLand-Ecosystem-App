450639ef43205a130992835dbc9aca4bc9457e95
sifat-cse-bd <sifatbd933@gmail.com>
2026-05-14 03:49:28 +0600

    Update registration UI and add live duplicate validation

--- app/src/main/java/com/example/villageconnect/OnBoarding.kt
+++ app/src/main/java/com/example/villageconnect/OnBoarding.kt
@@ -4,10 +4,8 @@ import android.content.Intent
 import android.graphics.Color
 import android.net.Uri
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
 import android.widget.FrameLayout
-import android.widget.LinearLayout
 import android.widget.TextView
 import android.widget.VideoView
 import androidx.activity.enableEdgeToEdge
@@ -15,6 +13,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.villageconnect.auth.Registration
 import com.example.villageconnect.fragments.AppOverview
 import kotlin.jvm.java
 
